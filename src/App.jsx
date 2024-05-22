56ac98fbfec681b2609bacb57aee3aab45518a52
olanrewaju <salaudeenwarees9@gmail.com>
2024-05-22 22:22:34 +0100

    the react quiz

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import DateCounter from "./DateCounter";
+import Header from "./Header";
 
 function App() {
   return (
-    <>
-      <div>
-        <DateCounter />
-      </div>
-    </>
+    <div className="app">
+      <Header reactLogo={reactLogo} viteLogo={viteLogo} />
+
+      <main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </main>
+    </div>
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header({ reactLogo, viteLogo }) {
+  return (
+    <header className="app-header">
+      <img src={reactLogo} alt="React logo" />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
